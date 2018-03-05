a6b5a65a37073bbdd9c8df52bf372f0f05924178
dn046118 <dhinesh.nakkeerar@cerner.com>
2018-03-05 10:03:54 -0600

    testMySetIsExistsNonEmptySet

--- src/pivotal/rerevised/MySet.java
+++ src/pivotal/rerevised/MySet.java
@@ -2,7 +2,25 @@ package pivotal.rerevised;
 
 public class MySet {
 
-	public boolean get(int item) {
+	int[] dataStore;
+	int lastIndex;
+
+	public MySet(int[] is) {
+		dataStore = is;
+		lastIndex = dataStore.length-1;
+	}
+	
+	public MySet(){
+		dataStore = new int[10];
+		lastIndex = -1;
+	}
+
+	public boolean isExists(int item) {
+		for(int i=0; i<lastIndex+1; i++){
+			if(dataStore[i]==item){
+				return true;
+			}
+		}
 		return false;
 	}
 
--- src/pivotal/rerevised/MySetTest.java
+++ src/pivotal/rerevised/MySetTest.java
@@ -7,9 +7,17 @@ import org.junit.Test;
 public class MySetTest {
 
 	@Test
-	public void testMySetGetEmptySet() {
+	public void testMySetIsExistsEmptySet() {
 		MySet mySet = new MySet();
-		assertEquals(false, mySet.get(1));
+		assertEquals(false, mySet.isExists(1));
 	}
+	
+	@Test
+	public void testMySetIsExistsNonEmptySet(){
+		MySet mySet = new MySet(new int[]{1});
+		assertEquals(true, mySet.isExists(1));
+		assertEquals(false, mySet.isExists(2));
+	}
+	
 
 }
